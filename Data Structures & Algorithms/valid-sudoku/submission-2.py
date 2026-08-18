3f7e6ebaa00ae723a6caae433066155f737d58d9
Lingge824 <252989533+Lingge824@users.noreply.github.com>
2026-08-18 00:41:48 -0700

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row=defaultdict(set)
+        col=defaultdict(set)
+        squ=defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c]=='.':
+                    continue
+                if board[r][c] in row[r]:
+                    return False
+                if board[r][c] in col[c]:
+                    return False
+                if board[r][c] in squ[r//3,c//3]:
+                    return False
+
+                row[r].add(board[r][c])
+                col[c].add(board[r][c])
+                squ[r//3,c//3].add(board[r][c])
+        return True
